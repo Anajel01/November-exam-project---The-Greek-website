013ff7b14e1df58bd38837806ed7d98f2c6e9867
Anajel01 <150655411+Anajel01@users.noreply.github.com>
2024-11-12 20:30:44 +0100

    added a comment to test if the files are being correctly added

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # November-exam-project---The-Greek-website
+This is the most basic structure of the website so far, we will be adding more changed accordingly
---
+++ script.js
@@ -0,0 +1,9 @@
+// JavaScript function to toggle the burger menu
+function toggleMenu() {
+    document.querySelector('.burger-menu').classList.toggle('active');
+  }
+  function toggleMenu() {
+    const mobileNav = document.getElementById("mobile-nav");
+    mobileNav.style.display = mobileNav.style.display === "flex" ? "none" : "flex";
+  }
+    
